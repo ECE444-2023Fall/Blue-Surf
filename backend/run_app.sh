2f6b9656d6ca02ac7a108c8287072da1f1941519
meriam04 <meriam_fourati@yahoo.com>
2023-10-28 21:09:51 -0400

    Set up docker for the flask app. Build and run the docker container using the ./run_app.sh script

---
+++ backend/run_app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the Docker image name and app port
+IMAGE_NAME="backend_docker"
+APP_PORT=3000
+
+# Check if the Docker image already exists
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]] || [ "$1" == "--rebuild" ]; then
+  # Build the Docker image if it doesn't exist or the rebuild flag is provided
+  docker build -t $IMAGE_NAME .
+fi
+
+# Run the Docker container
+docker run -d -p $APP_PORT:5000 $IMAGE_NAME
+
+# Provide instructions to access the app
+echo "Flask app is running. You can access it at http://localhost:$APP_PORT"
