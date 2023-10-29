810f866d60b88162cbde3e942d52e98b658c2dc0
meriam04 <meriam_fourati@yahoo.com>
2023-10-29 15:31:44 -0400

    Updated the run_app.sh script to check that docker is running first. It also will check if an image with the same name already exists.

--- backend/run_app.sh
+++ backend/run_app.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if Docker is installed and running
+if ! command -v docker &> /dev/null || ! docker info &> /dev/null; then
+  echo "Docker is not installed or not running. Please install and start Docker before running this script."
+  exit 1
+fi
+
 # Define the Docker image name and app port
 IMAGE_NAME="backend_docker"
 APP_PORT=4999
@@ -8,6 +14,17 @@ APP_PORT=4999
 if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]] || [ "$1" == "--rebuild" ]; then
   # Build the Docker image if it doesn't exist or the rebuild flag is provided
   docker build -t $IMAGE_NAME .
+fi
+
+# Check if a container with the same name is already running
+if [[ "$(docker ps -q -f name=$IMAGE_NAME)" != "" ]]; then
+  # Stop the existing container
+  docker stop $IMAGE_NAME
+fi
+
+# Remove the existing container
+if [[ "$(docker ps -aq -f name=$IMAGE_NAME)" != "" ]]; then
+  docker rm $IMAGE_NAME
 fi
 
 # Run the Docker container
