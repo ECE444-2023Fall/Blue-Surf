91e9306449d03d847c65ea558f67adb9e7767fe7
Paula Perdomo <paulaperdomo@Paulas-MacBook-Pro.local>
2023-10-29 12:38:51 -0400

    Adding a test case to the App.test.js to expect no search before. Will change once landing page is done

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('displays "No search results available." message on initial load', () => {
+  render(<App />);
+  const messageElement = screen.getByText(/No search results available./i);
+  expect(messageElement).toBeInTheDocument();
+});
