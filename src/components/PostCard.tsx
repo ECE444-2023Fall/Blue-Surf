9c9ee0931eb8c22243f65aa3c2aec00ff9024ab8
Karishma Shah <shah.karishma02@gmail.com>
2023-10-30 14:39:56 -0400

    add the favourite icon to postCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 import "../styles/PostCard.css";
 const postImage = require("../assets/post1.jpeg");
 
@@ -12,10 +13,20 @@ interface PostCardProps {
 }
 
 const PostCard: React.FC<PostCardProps> = (PostCardProps: any) => {
+  const [isLiked, setIsLiked] = React.useState(false);
+
+  const toggleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="col">
       <div className="card">
-        <img src={postImage} className="card-img-top rounded-top-34" alt="..." />
+        <img
+          src={postImage}
+          className="card-img-top rounded-top-34"
+          alt="..."
+        />
         <div className="card-body">
           <div className="d-flex justify-content-center">
             <span className="h4 card-title text-center">
@@ -26,11 +37,25 @@ const PostCard: React.FC<PostCardProps> = (PostCardProps: any) => {
             {PostCardProps.date.toDateString()} | {PostCardProps.location}
           </p>
           <p className="card-text text-left">{PostCardProps.description}</p>
-          <span className="pill">
-            {PostCardProps.tags.map((tag: string, index: number) => (
-              <span className="pill-tag" key={index}>{tag}</span>
-            ))}
-          </span>
+          <div className="row">
+            <div className="col">
+              <span className="pill">
+                {PostCardProps.tags.map((tag: string, index: number) => (
+                  <span className="pill-tag" key={index}>
+                    {tag}
+                  </span>
+                ))}
+              </span>
+            </div>
+            <div className="col-auto">
+              <button
+                className={`like-button ${isLiked ? "liked" : ""}`}
+                onClick={toggleLike}
+              >
+                <i className={`fa fa-heart${isLiked ? "" : "-o"}`} />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
