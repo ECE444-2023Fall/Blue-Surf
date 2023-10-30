c156c23e8c755816394fa663dfcf5aefa1a9e6e7
Karishma Shah <shah.karishma02@gmail.com>
2023-10-30 17:00:10 -0400

    add the navbar component and related styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PostCard from "./components/PostCard";
 import FilterField from "./components/FilterField";
-import SortBy from "./components/SortBy"; 
+import SortBy from "./components/SortBy";
+import FNavbar from "./components/FNavbar";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // this is mock data, to be replaced later once database is setup
 const postCardData = {
@@ -43,31 +45,34 @@ const numberOfCards = 10;
 
 function App() {
   return (
-    <div className="custom-container">
-      <div className="row">
-        <div className="custom-col-md-3">
-          {filterOptions.map((option, index) => (
-            <FilterField
-              key={index}
-              title={option.title}
-              values={option.values}
-            />
-          ))}
-        </div>
-        <div className="col-md-9">
-          <div className="row">
-            <div className="col-12">
-              <SortBy options={["Sort Option 1", "Sort Option 2"]} />
-            </div>
-          </div>
-          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 gx-3 gy-3">
-            {Array.from({ length: numberOfCards }).map((_, index) => (
-              <PostCard key={index} {...postCardData} />
+    <Router>
+      <div className="custom-container">
+        <FNavbar />
+        <div className="row">
+          <div className="custom-col-md-3">
+            {filterOptions.map((option, index) => (
+              <FilterField
+                key={index}
+                title={option.title}
+                values={option.values}
+              />
             ))}
+          </div>
+          <div className="col-md-9">
+            <div className="row">
+              <div className="col-12">
+                <SortBy options={["Sort Option 1", "Sort Option 2"]} />
+              </div>
+            </div>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 gx-3 gy-3">
+              {Array.from({ length: numberOfCards }).map((_, index) => (
+                <PostCard key={index} {...postCardData} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/FNavbar.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "../styles/FNavbar.css";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+interface User {
+  displayName: string;
+}
+
+function FNavbar() {
+  // Test user placeholder until Auth is set up
+  const [user, setUser] = useState<User | null>(null);
+
+  const logIn = () => {
+    // TODO: Implement sign-in logic, e.g., using an authentication service.
+    setUser({
+      displayName: "Test User",
+    });
+  };
+
+  const logOut = () => {
+    // TODO: Implement sign-out logic.
+    setUser(null);
+  };
+
+  let renderNav: JSX.Element;
+
+  if (user) {
+    renderNav = (
+      <Nav className="right-align">
+        <LinkContainer to="/dashboard">
+          <Nav.Link className="navbar-link-text">Dashboard</Nav.Link>
+        </LinkContainer>
+        <Nav.Link className="navbar-link-text" onClick={logOut}>
+          Sign Out
+        </Nav.Link>
+        <p className="navbar-link-text my-2">
+          {" "}
+          <strong>{user.displayName}</strong>{" "}
+        </p>
+      </Nav>
+    );
+  } else {
+    renderNav = (
+      <div className="right-align">
+        <LinkContainer to="/signin">
+          <Nav.Link className="navbar-link-text bold" onClick={logIn}>
+            Login / Sign Up
+          </Nav.Link>
+        </LinkContainer>
+      </div>
+    );
+  }
+
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="none"
+      variant="light"
+      fixed="top"
+      className="mynavbar py-md-3 px-md-5 px-3 py-2"
+    >
+      <LinkContainer to="/" className="navbar-brand-container">
+        <Navbar.Brand className="navbar-brand">
+          BLUESURF <span style={{ fontSize: "1.5em" }}>🏄‍♂️</span>
+        </Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        className="mynavbar-toggle"
+      />
+      <Navbar.Collapse id="responsive-navbar-nav">{renderNav}</Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default FNavbar;
