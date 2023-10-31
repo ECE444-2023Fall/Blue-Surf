acab0d7e0f38a081f07814b4989afb30a1aa4b24
Tenzin Mahabir <tmahabir10@gmail.com>
2023-10-31 14:51:43 -0400

    Moved mock events into a separate folder/file to reduce clutter in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,84 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
+from mock_data.mock_events import mockEvents
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config["SECRET_KEY"] = "NEED TO CHANGE"
-
-mockEvents = [
-  {
-    "event_id": 1,
-    "title": "Sample Event 1",
-    "description": "This is the first sample event.",
-    "location": "Sample Location 1",
-    "start_time": "2023-11-01T08:00:00",
-    "end_time": "2023-11-01T17:00:00",
-    "user_id": 1,
-    "is_published": True,
-    "is_public": True,
-    "like_count": 25
-  },
-  {
-    "event_id": 2,
-    "title": "Sample Event 2",
-    "description": "A different kind of event.",
-    "location": "Sample Location 2",
-    "start_time": "2023-11-05T10:00:00",
-    "end_time": "2023-11-05T16:00:00",
-    "user_id": 2,
-    "is_published": True,
-    "is_public": True,
-    "like_count": 10
-  },
-  {
-    "event_id": 3,
-    "title": "Another Event",
-    "description": "Details about another event.",
-    "location": "Sample Location 3",
-    "start_time": "2023-11-10T12:00:00",
-    "end_time": "2023-11-10T20:00:00",
-    "user_id": 1,
-    "is_published": True,
-    "is_public": True,
-    "like_count": 30
-  },
-  {
-    "event_id": 4,
-    "title": "Tech Conference 2023",
-    "description": "Join us for the latest in tech trends.",
-    "location": "Convention Center",
-    "start_time": "2023-11-15T09:00:00",
-    "end_time": "2023-11-17T18:00:00",
-    "user_id": 3,
-    "is_published": True,
-    "is_public": True,
-    "like_count": 75
-  },
-  {
-    "event_id": 5,
-    "title": "Music Festival",
-    "description": "A three-day music extravaganza.",
-    "location": "Outdoor Arena",
-    "start_time": "2023-11-20T16:00:00",
-    "end_time": "2023-11-22T23:00:00",
-    "user_id": 2,
-    "is_published": True,
-    "is_public": True,
-    "like_count": 120
-  },
-  {
-    "event_id": 6,
-    "title": "Local Charity Run",
-    "description": "Support a good cause while staying fit.",
-    "location": "City Park",
-    "start_time": "2023-11-25T07:30:00",
-    "end_time": "2023-11-25T11:00:00",
-    "user_id": 4,
-    "is_published": True,
-    "is_public": True,
-    "like_count": 40
-  }
-]
 
 def get_matched_events(query, detailed=False):
     if not query:
