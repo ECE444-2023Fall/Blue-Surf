fd1a138ef1912e6b4871fe85eedf332cb465ad91
divyaK15 <divya.p.k15@gmail.com>
2023-11-05 14:57:12 -0500

    adding profile page

---
+++ src/components/Profile.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import axios from "axios";
+
+function Profile(props) {
+
+  const [profileData, setProfileData] = useState(null)
+  function getData() {
+    axios({
+      method: "GET",
+      url:"/profile",
+      headers: {
+        Authorization: 'Bearer ' + props.token
+      }
+    })
+    .then((response) => {
+      const res =response.data
+      res.access_token && props.setToken(res.access_token)
+      setProfileData(({
+        profile_name: res.name,
+        about_me: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
+
+  return (
+    <div className="Profile">
+
+        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        {profileData && <div>
+              <p>Profile name: {profileData.profile_name}</p>
+              <p>About me: {profileData.about_me}</p>
+            </div>
+        }
+
+    </div>
+  );
+}
+
+export default Profile;
