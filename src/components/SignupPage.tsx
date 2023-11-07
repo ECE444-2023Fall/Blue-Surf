e5717949d7dd91d72a58a82754f2e87d1f60ace7
divyaK15 <divya.p.k15@gmail.com>
2023-11-07 17:28:54 -0500

    reverting changes

--- src/components/SignupPage.tsx
+++ src/components/SignupPage.tsx
@@ -60,13 +60,6 @@ const SignupPage: React.FC = () => {
       return;
     }
 
-    if(!/^\S+@\S+\.\S+$/.test(registerForm.email)){
-      return;
-    }
-
-    if(!registerForm.email.includes("utoronto.ca")){
-      return; 
-    }
 
     try {
       const response = await fetch("/api/register", {
