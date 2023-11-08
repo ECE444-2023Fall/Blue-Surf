af1f69162da69d42cd56d6de5f04fb758c6bbf64
Tenzin Mahabir <tmahabir10@gmail.com>
2023-11-08 01:54:10 -0500

    Added PostCreatePage. Updated AutoSizeTextArea component to take in placeholder in pro p. Will need to update PostDetailsPage with this as that shouuld have placeholders too.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import FNavbar from "./components/FNavbar";
 import LandingPage from "./components/LandingPage";
 import PostDetailsPage from "./components/PostDetailsPage";
+import PostCreatePage from "./components/PostCreatePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/post/:postId" element={<PostDetailsPage />} />
+          <Route path="/create" element={<PostCreatePage />} />
           {/* Define other routes here*/}
         </Routes>
       </div>
---
+++ src/components/PostCreatePage.tsx
@@ -0,0 +1,241 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "../styles/PostDetailsPage.css";
+import "../styles/PostCreatePage.css";
+import AutoSizeTextArea from "./AutoSizeTextArea";
+const imageTemplate = require("../assets/post-template.jpg");
+//<a href="https://www.freepik.com/free-vector/blue-fluid-patterned-background-vector_18896232.htm#query=wave&position=6&from_view=search&track=sph">Image by rawpixel.com</a> on Freepik
+
+interface Post {
+  title: string;
+  start_time: Date;
+  location: string;
+  description: string;
+  extended_description: string;
+  tags: string[];
+  id: number;
+  author_id: number;
+  is_published: boolean;
+  end_time: Date;
+  like_count: number;
+  club?: string;
+}
+
+const PostCreatePage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [editedPost, setEditedPost] = useState<Post>({
+    title: "Catchy Event Name Here 🎉",
+    start_time: new Date(),
+    location: "Where's the action happening? 📍",
+    description: "Describe the excitement in a few words! 🌟",
+    extended_description: "Dive deeper into your event/club here! 🌊",
+    tags: [],
+    id: 0,
+    author_id: 1,
+    is_published: true,
+    end_time: new Date(),
+    like_count: 0,
+    club: "",
+  });
+  const [imageSrc, setImageSrc] = useState(imageTemplate);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const handleSave = async () => {
+    try {
+      if (!editedPost.title && !editedPost.location) {
+        setErrorMessage("Title and Location are required fields.");
+        return;
+      } else if (!editedPost.title) {
+        setErrorMessage("Title is a required field.");
+        return;
+      } else if (!editedPost.location) {
+        setErrorMessage("Location is a required field.");
+        return;
+      }
+
+      const formattedStartDate = editedPost.start_time
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ");
+      const formattedEndDate = editedPost.end_time
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ");
+
+      const postData = {
+        ...editedPost,
+        start_time: formattedStartDate,
+        end_time: formattedEndDate,
+      };
+
+      const response = await fetch(`/api/create-post`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+
+      if (response.ok) {
+        navigate("/dashboard");
+      } else {
+        const data = await response.json();
+        throw new Error(data["error message"]);
+      }
+    } catch (error) {
+      console.error("Create Post Error:", error);
+    }
+  };
+
+  const handleCancel = () => {
+    navigate(-1);
+  };
+
+  const handleFileChange = (event: any) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const newImageSrc = e.target && e.target.result;
+        setImageSrc(newImageSrc);
+      };
+
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
+  return (
+    <div className="container background-colour rounded-5 p-5 mt-2 mb-2">
+      <div className="row m-2">
+        <a className="navbar-brand back-nav" href="javascript:history.back()">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/271/271220.png"
+            width="15"
+            height="15"
+            className="d-inline-block align-items-center"
+            alt=""
+          />
+          <span className="back-text">Back</span>
+        </a>
+        <div className="row m-2 justify-content-end">
+          <>
+            {/* Display the error message */}
+            {errorMessage && (
+              <div className="error-message">{errorMessage}</div>
+            )}
+            <button className="cancel-button" onClick={handleCancel}>
+              Cancel
+            </button>
+            <button className="edit-button" onClick={handleSave}>
+              Post
+            </button>
+          </>
+        </div>
+      </div>
+
+      <div className="row g-5 m-2">
+        <div className="col-md-6">
+          <img src={imageSrc} className="card-img-top rounded-edge" alt="..." />
+          <div className="row g-5 m-2 d-flex justify-content-center">
+            <>
+              <input
+                type="file"
+                id="fileInput"
+                className="hidden-input"
+                accept="image/*"
+                onChange={handleFileChange}
+              />
+              <label htmlFor="fileInput" className="custom-file-input">
+                Choose a File
+              </label>
+            </>
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <div className="container-styling">
+            <div className="title">
+              <AutoSizeTextArea
+                content={editedPost.title}
+                onChange={(value) =>
+                  setEditedPost({ ...editedPost, title: value })
+                }
+                placeholderWord="[enter title here]"
+              />
+            </div>
+            <div className="summary">
+              <AutoSizeTextArea
+                content={editedPost.description}
+                onChange={(value) =>
+                  setEditedPost({ ...editedPost, description: value })
+                }
+                placeholderWord="[enter description here]"
+              />
+            </div>
+            {/* <span className="pill">
+              {post.tags.map((tag: string, index: number) => (
+                <span className="pill-tag" key={index}>
+                  {tag}
+                </span>
+              ))}
+            </span> */}
+            <div className="subtitle">About</div>
+            <div className="details">
+              {/* TODO: replace with extendedDescription field */}
+              <AutoSizeTextArea
+                content={editedPost.extended_description}
+                onChange={(value) =>
+                  setEditedPost({
+                    ...editedPost,
+                    extended_description: value,
+                  })
+                }
+                placeholderWord="[enter extended description here]"
+              />
+            </div>
+            <div className="subtitle">Date</div>
+            <div className="details">
+              <AutoSizeTextArea
+                content={editedPost.start_time.toLocaleString()}
+                onChange={(value) =>
+                  setEditedPost({
+                    ...editedPost,
+                    start_time: new Date(value),
+                  })
+                }
+              />
+            </div>
+            <div className="subtitle">Location</div>
+            <div className="details">
+              <AutoSizeTextArea
+                content={editedPost.location}
+                onChange={(value) =>
+                  setEditedPost({ ...editedPost, location: value })
+                }
+                placeholderWord="[enter location here]"
+              />
+            </div>
+            <div>
+              <div className="subtitle">Club</div>
+              <div className="details">
+                <AutoSizeTextArea
+                  content={editedPost.club || ""}
+                  onChange={(value) =>
+                    setEditedPost({ ...editedPost, club: value })
+                  }
+                  placeholderWord="[enter club name here if applicable]"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostCreatePage;
