9eff4eb235c53e666b366905933e02b98fea563c
meriam04 <meriam_fourati@yahoo.com>
2023-11-09 00:43:43 -0500

    Updated api calls to go to deployed backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:5000',
+      target: 'https://bluesurf.onrender.com/',
       changeOrigin: true,
     })
   );
