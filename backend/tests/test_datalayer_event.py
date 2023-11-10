2915f55900dd87b0cf4b80fd4770c374366e4041
meriam04 <meriam_fourati@yahoo.com>
2023-11-10 15:50:21 -0500

    Validated datalayer image inputs in the database

--- backend/tests/test_datalayer_event.py
+++ backend/tests/test_datalayer_event.py
@@ -1,6 +1,10 @@
 import sys
+import os
 import pytest
 import logging
+from PIL import Image
+import io
+from pathlib import Path
 
 from .test_datalayer import test_client
 
@@ -25,6 +29,12 @@ def test_event_creation(test_client):
     tag.add_tag("Tag 1")
 
     event = EventDataLayer()
+
+    # Read the image file
+    image_file_path = os.path.join("../../images", "logo.png")
+    with open(image_file_path, "rb") as image_file:
+        image_data = image_file.read()
+
     try:
         event.create_event(
             title="Event 1",
@@ -36,7 +46,7 @@ def test_event_creation(test_client):
             author_name="testuser1",
             club="Club 1",
             is_published=True,
-            image=None,
+            image=image_data,
             tags=["Tag 1"],
         )
     except ValueError as value_error:
@@ -362,6 +372,11 @@ def test_event_update(test_client):
         image=None,
     )
     try:
+        # Read the image file
+        image_file_path = os.path.join("../../images", "logo.png")
+        with open(image_file_path, "rb") as image_file:
+            image_data = image_file.read()
+
         with app.app_context():
             event_id = Event.query.filter_by(title="Event 1").first().id
         event.update_event(
@@ -371,6 +386,7 @@ def test_event_update(test_client):
             extended_description="Extended decription for event 1 CHANGED for club 1 that is much longer than just the description",
             location="Toronto",
             tags=["Tag 1"],
+            image=image_data,
         )
 
     except ValueError as value_error:
@@ -386,6 +402,18 @@ def test_event_update(test_client):
         assert new_event.title == "Event 1 - CHANGED"
         assert len(new_event.tags) == 1
         assert new_event.tags[0].name == "Tag 1"
+
+        image_data = new_event.image
+        image = Image.open(io.BytesIO(image_data))
+
+        subdirectory_name = "output_images"
+
+        # Create the subdirectory if it doesn't exist
+        output_directory = Path.cwd() / subdirectory_name
+        output_directory.mkdir(parents=True, exist_ok=True)
+        output_image_path = output_directory / "retrieved_image.png"
+        # Save the image to a file
+        image.save(output_image_path)
 
 
 def test_event_update_delete_tag(test_client):
@@ -575,7 +603,7 @@ def test_get_tag_ids_for_event(test_client):
         password_hash="testpassword",
         password_salt="testpassword",
     )
-    
+
     tag = TagDataLayer()
     tag.add_tag("Tag 1")
 
