8603f76e35b18fa1dc2a1d5e10f7139480bffd65
meriam04 <meriam_fourati@yahoo.com>
2023-11-11 17:16:36 -0500

    Fixed login reroute issue

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import "../styles/LoginPage.css";
+import API_URL from '../config';
 const surfEmojiImage = require("../assets/surf-emoji.png");
 const waveImage = require("../assets/wave.png");
 
